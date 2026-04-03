55f41c1db1e7af93b2f832e900826c90d2f950c2
Andrew Motko <andrew.motko@gmail.com>
2026-04-04 00:22:11 +0300

    feat: add logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { ArticleModule } from './article/article.module';
 import { CommentModule } from './comment/comment.module';
+import { LoggingMiddleware } from './common/logging.middleware';
 
 @Module({
   imports: [UserModule, CategoryModule, ArticleModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
